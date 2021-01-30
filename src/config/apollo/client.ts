b3dfff4f886895ff265ce8b33dd8db947e1914c1
Dmitrii Kartashev <dmitrii@freightpath.io>
2021-01-30 01:04:26 -0500

    chore: :wrench: use own API keys

--- src/config/apollo/client.ts
+++ src/config/apollo/client.ts
@@ -3,14 +3,12 @@ import { typePolicies } from "./typePolicies";
 
 const httpLink = new HttpLink({
   uri:
-    "https://cors-anywhere.herokuapp.com/https://welbi.org/api/graphql?token=1",
+    "https://cors-anywhere.herokuapp.com/https://welbi.org/api/graphql?token=f4df52a9-b9da-41af-9f12-887a3336fcf2",
 });
-
-
 
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache({
-    typePolicies
+    typePolicies,
   }),
 });
