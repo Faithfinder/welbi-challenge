255ffba440510483e798ed92bff5696950173919
Dmitrii Kartashev <faithfinder@yandex.ru>
2021-11-18 11:53:39 -0500

    Update client.ts

--- src/config/apollo/client.ts
+++ src/config/apollo/client.ts
@@ -3,7 +3,7 @@ import { typePolicies } from "./typePolicies";
 
 const httpLink = new HttpLink({
   uri:
-    "https://cors-anywhere.herokuapp.com/https://welbi.org/api/graphql?token=f4df52a9-b9da-41af-9f12-887a3336fcf2",
+    "https://welbi.org/api/graphql?token=f4df52a9-b9da-41af-9f12-887a3336fcf2",
 });
 
 export const client = new ApolloClient({
