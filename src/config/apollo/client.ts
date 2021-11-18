8e9165518f984c8b6608cfd65ee921caed748c63
Dmitrii Kartashev <faithfinder@yandex.ru>
2021-11-18 12:12:34 -0500

    Update client.ts

--- src/config/apollo/client.ts
+++ src/config/apollo/client.ts
@@ -3,7 +3,8 @@ import { typePolicies } from "./typePolicies";
 
 const httpLink = new HttpLink({
   uri:
-    "https://welbi.org/api/graphql?token=f4df52a9-b9da-41af-9f12-887a3336fcf2",
+    "https://welbi.org/api/graphql",
+  headers: { "Authorization": `Bearer f4df52a9-b9da-41af-9f12-887a3336fcf2` }
 });
 
 export const client = new ApolloClient({
